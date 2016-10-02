15fcacaa2309f910aec46bab7a875d59489923c2
へーさん <hira9603859504@gmail.com>
2016-10-02 22:46:01 +0900

    最初のコミット
    +タイルマップ
    +プレイヤーアニメーション(手前方向のみ)
    +1マスずつ動かす(アニメーター、手前方向のみ)

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+	Animator animator;
+	public float Vertical;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vertical = Input.GetAxisRaw ("Vertical");
+		if(Vertical < 0){
+			animator.SetBool("Walk_Down_key", true);
+		}else{
+			animator.SetBool("Walk_Down_key", false);
+		}
+	}
+}
