2bae2666d06446496011a256c883c9fa3dc4fdca
へーさん <hira9603859504@gmail.com>
2016-10-03 17:53:51 +0900

    上移動追加

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour {
 	Animator animator;
 	public float Vertical;
+	public float Horizontal;
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
@@ -13,10 +14,26 @@ public class PlayerMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vertical = Input.GetAxisRaw ("Vertical");
+		Horizontal = Input.GetAxisRaw ("Horizontal");
 		if(Vertical < 0){
 			animator.SetBool("Walk_Down_key", true);
 		}else{
 			animator.SetBool("Walk_Down_key", false);
+		}
+		if(Vertical > 0){
+			animator.SetBool("Walk_Up_key", true);
+		}else{
+			animator.SetBool("Walk_Up_key", false);
+		}
+		if(Horizontal < 0){
+			animator.SetBool("Walk_Right_key", true);
+		}else{
+			animator.SetBool("Walk_Right_key", false);
+		}
+		if(Horizontal > 0){
+			animator.SetBool("Walk_Left_key", true);
+		}else{
+			animator.SetBool("Walk_Left_key", false);
 		}
 	}
 }
